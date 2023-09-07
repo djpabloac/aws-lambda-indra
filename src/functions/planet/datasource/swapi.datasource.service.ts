69fd5c0c6a62fe24d48c44da8131bb0e2318c5c8
Pablo Arteaga <dj.pablo.ac@gmail.com>
2023-09-06 20:16:41 -0500

    fix: adding defensive code parse dto

--- src/functions/planet/datasource/swapi.datasource.service.ts
+++ src/functions/planet/datasource/swapi.datasource.service.ts
@@ -3,18 +3,25 @@ import { CreatePlanetDto } from "../dto/create-planet.dto"
 import { FinderByIdDto } from "../dto/finder-by-id.dto"
 import { PlanetExternal } from "./planet.api.entity"
 
-const mapPlanetExternalToPlanet = (raw: PlanetExternal): CreatePlanetDto => ({
-  nombre: raw.name,
-  periodo_rotacion: parseInt(raw.rotation_period),
-  periodo_orbital: parseInt(raw.orbital_period),
-  diametro: parseInt(raw.diameter),
-  clima: raw.climate,
-  gravedad: raw.gravity,
-  terreno: raw.terrain,
-  agua_superficial: parseInt(raw.surface_water),
-  poblacion: parseInt(raw.population),
-  url: raw.url
-})
+const mapPlanetExternalToPlanet = (raw: PlanetExternal): CreatePlanetDto => {
+  const rotation_period = parseInt(raw.rotation_period)
+  const orbital_period = parseInt(raw.orbital_period)
+  const diameter = parseInt(raw.diameter)
+  const population = parseInt(raw.population)
+
+  return {
+    nombre: raw.name,
+    periodo_rotacion: isNaN(rotation_period) ? 0 : rotation_period,
+    periodo_orbital: isNaN(orbital_period) ? 0 : orbital_period,
+    diametro: isNaN(diameter) ? 0 : diameter,
+    clima: raw.climate,
+    gravedad: raw.gravity,
+    terreno: raw.terrain,
+    agua_superficial: parseInt(raw.surface_water),
+    poblacion: isNaN(population) ? 0 : population,
+    url: raw.url
+  }
+}
 
 export const getPlanetOfSwapiById = async (finderById: FinderByIdDto): Promise<CreatePlanetDto> => {
   const data = await SwapiApi.getOne<PlanetExternal>(`/planets/${finderById.id}`)
